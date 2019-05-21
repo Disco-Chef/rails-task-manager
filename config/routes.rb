f60da592cefea8a7efbfe2db12b5f93c58c7fbf7
Nikos Agathos <nicolas.agathos@gmail.com>
2019-05-21 16:34:24 +0200

    task crud challenge mostly complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # INDEX
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: :tasks
   # NEW
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: :task_new
   # CREATE
   post 'tasks', to: 'tasks#create'
   # SHOW
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/:id', to: 'tasks#show', as: :task
   # EDIT
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
   # UPDATE
   patch 'tasks/:id', to: 'tasks#update'
   # DESTROY
