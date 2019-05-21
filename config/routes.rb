f4e4dd61b76ccf4543712590c216ff0109bc74b5
Nikos Agathos <nicolas.agathos@gmail.com>
2019-05-21 14:05:49 +0200

    added routes and empty method definitions in Tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # index
+  get 'tasks', to: 'tasks#index'
+  # new
+  get 'tasks/new', to: 'tasks#new'
+  # create
+  post 'tasks', to: 'tasks#create'
+  # show
+  get 'tasks/:id', to: 'tasks#show'
+  # edit
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  # update
+  patch 'restaurants/:id', to: 'restaurants#update'
+  # destroy
+  delete 'restaurants/:id', to: 'restaurants#destroy'
 end
